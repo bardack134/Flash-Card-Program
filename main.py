03df505308cbc9b54a4aba7f06b4001202b6968a
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-03-19 12:51:29 +0900

    cambiando la imagen de fondo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from PIL import Image, ImageTk
 
 #CONSTANTES
 BACKGROUND_COLOR = "#B1DDC6"
@@ -25,22 +25,31 @@ my_canvas=Canvas(window, bg=BACKGROUND_COLOR,  width=800,  height=526, highlight
 my_canvas.grid(row=0, column=1, columnspan=2)
 
 
-#cargo la imagen
-card_front_imagen=PhotoImage(file="card_front.png")
+#mi foto esta demasiodo grande por lo que usare el modulo PIL(python image libray para redimencionarla)
+front_image=Image.open("img1.png")
+
+
+#con el comando resize podemos redimencionar nuestras imagenes
+front_image_resized = front_image.resize((800, 526), Image.Resampling.LANCZOS)
+
+
+#usamos nuestra 'front_image_resized' que hasido redimencionada
+#Usamos ImageTk.PhotoImage para convertirlo en un formato compatible con Tkinter antes de poder usarlo
+card_front_image = ImageTk.PhotoImage(front_image_resized)
 
 
 #creo mi foto dentro del canvas
-my_canvas.create_image(400, 263, image=card_front_imagen, anchor='center')
+my_canvas.create_image(400, 263, image=card_front_image, anchor='center')
 # my_canvas.create_image(0, 20, image=card_front_imagen, anchor='nw')
 
 
 #labels donde iran las palabras del vocabulario
-title_label=Label(my_canvas, text='Title', font=("Ariel", 40, "italic"), bg='white')
-title_label.place(x=400, y=150, anchor='center')
+title=my_canvas.create_text(400,150, text='Title' , fill='white', font=("Ariel", 40, "italic"))
 
 
-word_label=Label(my_canvas, text='Word', bg='white', font=("Ariel", 60, "bold"))
-word_label.place(x=400, y=263, anchor='center')
+
+word=my_canvas.create_text(400, 253, text='Word', fill='white', font=("Ariel", 60, "bold"))
+
 
 
 #para correr nuestra app
